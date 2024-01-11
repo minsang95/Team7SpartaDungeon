9473f44c5e1d8c6aeaa63345adf8554955bed368
wjj329 <wjj2187@naver.com>
2024-01-11 12:57:21 +0900

    feat. JungJin 네임스페이스 추가
    Jungjin. cs 추가

---
+++ Team JungJin/Jungjin.cs
@@ -0,0 +1,10 @@
+﻿namespace Team_JungJin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
